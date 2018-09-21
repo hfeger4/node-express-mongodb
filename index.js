ef2dc1a7d17182eb0d7ce3cae31fb13eb234e24d
Howard Feger <hfeger4@gmail.com>
2018-09-21 14:55:14 -0700

    changed greeting

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express(); //the server
 //req stands for request object
 //res stands for response object
 app.get('/', (req,res)=> {
-  res.send({hi: 'there'});
+  res.send({lol: 'no'});
 });
 
 const PORT = process.env.PORT || 3000;
