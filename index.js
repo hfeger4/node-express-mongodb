888438aa5b75997806264bc78f3c8abfc2895cff
Howard Feger <hfeger4@gmail.com>
2018-09-24 15:29:13 -0700

    set up initial oauth route

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
-const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys");
 
 const app = express(); //the server
 
-passport.use(new GoogleStrategy());
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
+);
 //console.developers.google.com
 
+//route handlers
+app.get(
+  "/auth/google",
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+);
 
 //req stands for request object
 //res stands for response object
@@ -14,9 +33,5 @@ passport.use(new GoogleStrategy());
 //   res.send({lol: 'no'});
 // });
 
-
-
-
-
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);
