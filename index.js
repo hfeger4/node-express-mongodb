a7f3dbda0673870e8ee44547faf8a65007d838b7
Howard Feger <hfeger4@gmail.com>
2018-09-24 13:04:26 -0700

    add passport

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express(); //the server
+
+passport.use(new GoogleStrategy());
+//console.developers.google.com
+
 
 //req stands for request object
 //res stands for response object
-app.get('/', (req,res)=> {
-  res.send({lol: 'no'});
-});
+// app.get('/', (req,res)=> {
+//   res.send({lol: 'no'});
+// });
+
+
+
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT);
